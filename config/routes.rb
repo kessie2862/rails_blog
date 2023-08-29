3b5221287898d5059ff658a946f93e2dc94454f4
kessie <kayp3477@gmail.com>
2023-08-29 14:38:10 +0000

    Init commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "pages#home"
+
+  get "about", to: "pages#about"
+  # get "pages/about" to: page.about
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
+end
